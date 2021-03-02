3a9c27ad00d7fdf34d3ede60ea4c266e2b18e080
kishan chaurasiya <kishan.chaurasiya.75@gmail.com>
2021-03-02 12:39:13 +0530

    allowed cors

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var auth = require('./controller').authorization;
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
 var app = express();
 
 // view engine setup
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
-require('dotenv').config();
+require("dotenv").config();
 
-
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 module.exports = app;
