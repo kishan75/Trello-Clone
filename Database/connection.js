8136eb8d9ac9ae1814c4c0a18839de21ce699293
kishan chaurasiya <kishan.chaurasiya.75@gmail.com>
2021-03-02 11:11:38 +0530

    removed hardcoded db name and added as a environment variable

--- Database/connection.js
+++ Database/connection.js
@@ -1,16 +1,17 @@
 var mongoose = require("mongoose");
 const USER_NAME = process.env.USER_NAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
-const URI = `mongodb+srv://${USER_NAME}:${DB_PASSWORD}@cluster0.9qht3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${USER_NAME}:${DB_PASSWORD}@cluster0.9qht3.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 const connectDB = async () => {
-    await mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    });
-    console.log('DB has connected');
+  await mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  console.log("DB has connected");
 };
 
 module.exports = connectDB;
